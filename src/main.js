af6d97562b78b09df35ceaa82f2c71fc3eea69fa
mr_ro0t <peter.schneubel@gmail.com>
2020-03-28 00:04:44 +0100

    included bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
 
 import "@/assets/css/tailwind.css";
+import "@/assets/css/custom.css"
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.filter("capitalize", function(value) {
   if (!value) return "";
