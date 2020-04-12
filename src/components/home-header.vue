4fdc93110d5f8090d7df4b241236fe40ecf68016
Bernd Braun <bernd.braun@code-to-web.com>
2020-04-12 23:43:35 +0200

    Re-Design Header subpages navigation

--- src/components/home-header.vue
+++ src/components/home-header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="fixed-top text-center mt-2 mt-sm-4">
         <b-button to="/" pill :variant="variant" size="lg" class="shadow-lg px-4">
-          <span class="text-secondary barcelony">Tafel</span>
+          <span class="text-secondary barcelony space">Tafel</span>
           <Logo class="logo"/>
           <span class="text-secondary barcelony">hilfe</span>
         </b-button>
