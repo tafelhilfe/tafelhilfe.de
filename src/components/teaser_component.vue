27be9b3aa5ac31c40de6e922dce0ff3ca6eafb61
mr_ro0t <peter.schneubel@gmail.com>
2020-04-13 20:18:33 +0200

    stage fixes

--- src/components/teaser_component.vue
+++ src/components/teaser_component.vue
@@ -2,8 +2,7 @@
     <b-row class="teaserItemContainer" v-if="teaser.item">
         <b-col lg class="bg-white teaserItem" v-for="item in teaser.item" :key="item.id">
             <div class="teaserIconContainer bg-secondary" v-if="item.icon">
-                <div class="teaserIcon desktop" :style="'background: url('+item.icon.url+'); background-repeat: no-repeat; background-size: 3rem 3rem'"/>
-                <div class="teaserIcon mobile" :style="'background: url('+item.icon.url+'); background-repeat: no-repeat; background-size: 5rem 5rem'"/>
+                <div class="teaserIcon" :style="'background-image: url('+item.icon.url+')'"/>
             </div>
             <div class="teaserContent text-tertiary" v-if="item.content">
                 <h3 class="mr-5" v-if="item.content.headline" v-html="item.content.headline"/>
@@ -19,3 +18,5 @@
         props: ['teaser']
     }
 </script>
+
+<style scoped
