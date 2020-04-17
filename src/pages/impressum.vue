48edc1c8010a9d2b01bf5ee2c4d62804303a90a3
Lennart Gastler <lennart.gastler@debab.de>
2020-04-18 00:02:01 +0200

    Updated DataPolicy and Impressum

--- src/pages/impressum.vue
+++ src/pages/impressum.vue
@@ -1,88 +1,111 @@
 <template>
-  <div class="flex flex-col min-h-screen content container">
-    <div class="relative bg-gray-50 overflow-hidden">
-      <div class="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
-        <div
-          class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28"
-        >
-          <div class="text-center">
-            <h2
-              class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-5xl"
-            >
-              Wir helfen euch neue Helfer zu finden!
-            </h2>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <div class="px-4 py-4 sm:px-0">
-        <h1 class="text-4xl">Legal Disclosure</h1>
-        Information in accordance with Section 5 TMG
-        <br />
-        <br />
-        Lennart Gastler
-        <br />
-        Am Seeacker 1
-        <br />
-        74821 Mosbach
-        <br />
-        <h2 class="text-3xl">Contact Information</h2>
-        Telephone: 0151/25896169
-        <br />
-        E-Mail:
-        <a class="underline text-highlight" href="mailto:kontakt@lgastler.de">
-          kontakt@lgastler.de
-        </a>
-        <br />
-        Internet address:
-        <router-link class="underline text-highlight" to="/" target="_blank">
-          www.lennartgastler.de
-        </router-link>
-        <br />
-        <br />
-        <h2 class="text-3xl">Disclaimer</h2>
-        <h3 class="text-xl">Accountability for content</h3>
-        <br />
-        The contents of our pages have been created with the utmost care.
-        However, we cannot guarantee the contents' accuracy, completeness or
-        topicality. According to statutory provisions, we are furthermore
-        responsible for our own content on these web pages. In this matter,
-        please note that we are not obliged to monitor the transmitted or saved
-        information of third parties, or investigate circumstances pointing to
-        illegal activity. Our obligations to remove or block the use of
-        information under generally applicable laws remain unaffected by this as
-        per §§ 8 to 10 of the Telemedia Act (TMG).
-        <br />
-        <br />
-        <h3 class="text-xl">Accountability for links</h3>
-        <br />
-        Responsibility for the content of external links (to web pages of third
-        parties) lies solely with the operators of the linked pages. No
-        violations were evident to us at the time of linking. Should any legal
-        infringement become known to us, we will remove the respective link
-        immediately.
-        <br />
-        <br />
-        <h3 class="text-xl">Copyright</h3>
-        <br />
-        Our web pages and their contents are subject to German copyright law.
-        Unless expressly permitted by law, every form of utilizing, reproducing
-        or processing works subject to copyright protection on our web pages
-        requires the prior consent of the respective owner of the rights.
-        Individual reproductions of a work are only allowed for private use. The
-        materials from these pages are copyrighted and any unauthorized use may
-        violate copyright laws.
+  <div class="content container">
+    <div class="mx-auto py-6">
+      <div class="px-4 py-4">
+        <h1>Impressum</h1>
+
+        <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+        <p>
+          Tafelhilfe e.V. i.G.<br />
+          Am Seeacker 1<br />
+          74821 Mosbach
+        </p>
+
+        <p>
+          <strong>Vertreten durch:</strong><br />
+          Lennart Gastler
+        </p>
+
+        <h2>Kontakt</h2>
+        <p>
+          Telefon: 004915125896169<br />
+          E-Mail: kontakt@tafelhilfe.de
+        </p>
+
+        <h2>EU-Streitschlichtung</h2>
+        <p>
+          Die Europ&auml;ische Kommission stellt eine Plattform zur
+          Online-Streitbeilegung (OS) bereit:
+          <a
+            href="https://ec.europa.eu/consumers/odr"
+            target="_blank"
+            rel="noopener noreferrer"
+            >https://ec.europa.eu/consumers/odr</a
+          >.<br />
+          Unsere E-Mail-Adresse finden Sie oben im Impressum.
+        </p>
+
+        <h2>
+          Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+        </h2>
+        <p>
+          Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+          vor einer Verbraucherschlichtungsstelle teilzunehmen.
+        </p>
+
+        <h3>Haftung f&uuml;r Inhalte</h3>
+        <p>
+          Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG
+          f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen
+          Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als
+          Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder
+          gespeicherte fremde Informationen zu &uuml;berwachen oder nach
+          Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit
+          hinweisen.
+        </p>
+        <p>
+          Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+          Informationen nach den allgemeinen Gesetzen bleiben hiervon
+          unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab
+          dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung
+          m&ouml;glich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen
+          werden wir diese Inhalte umgehend entfernen.
+        </p>
+        <h3>Haftung f&uuml;r Links</h3>
+        <p>
+          Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf
+          deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir
+          f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen.
+          F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige
+          Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten
+          Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche
+          Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte
+          waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+        </p>
+        <p>
+          Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
+          ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+          umgehend entfernen.
+        </p>
+        <h3>Urheberrecht</h3>
+        <p>
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+          Seiten unterliegen dem deutschen Urheberrecht. Die
+          Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der
+          Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes
+          bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw.
+          Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r den
+          privaten, nicht kommerziellen Gebrauch gestattet.
+        </p>
+        <p>
+          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
+          wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
+          Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf
+          eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+          entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+          werden wir derartige Inhalte umgehend entfernen.
+        </p>
+
+        <p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "imprint",
-  components: {
-  }
-};
+  name: 'Imprint',
+  components: {}
+}
 </script>
