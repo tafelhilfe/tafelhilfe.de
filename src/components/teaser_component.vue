3de2550aad2d6b25118fb243f915679680263e27
Lennart Gastler <lennart.gastler@debab.de>
2020-04-18 00:05:59 +0200

    Added Icons to TeaserComponent

--- src/components/teaser_component.vue
+++ src/components/teaser_component.vue
@@ -1,20 +1,29 @@
 <template>
-    <b-row class="teaserItemContainer" v-if="teaser.item">
-        <b-col lg class="bg-white teaserItem" v-for="item in teaser.item" :key="item.id">
-            <div class="teaserIconContainer bg-secondary" v-if="item.icon">
-                <div class="teaserIcon" :style="'background-image: url('+item.icon.url+')'"/>
-            </div>
-            <div class="teaserContent text-tertiary" v-if="item.content">
-                <h3 class="mr-5" v-if="item.content.headline" v-html="item.content.headline"/>
-                <p v-if="item.content.text" v-html="item.content.text"/>
-            </div>
-        </b-col>
-    </b-row>
+  <b-row v-if="teaser.item" class="teaserItemContainer">
+    <b-col
+      v-for="item in teaser.item"
+      :key="item.id"
+      lg
+      class="bg-white teaserItem"
+    >
+      <div v-if="item.icon" class="teaserIconContainer bg-secondary">
+        <div class="teaserIcon" v-html="item.icon" />
+      </div>
+      <div v-if="item.content" class="teaserContent text-tertiary">
+        <h3
+          v-if="item.content.headline"
+          class="mr-5"
+          v-html="item.content.headline"
+        />
+        <p v-if="item.content.text" v-html="item.content.text" />
+      </div>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
-    export default {
-        name: 'teaser_component',
-        props: ['teaser']
-    }
+export default {
+  name: 'TeaserComponent',
+  props: ['teaser']
+}
 </script>
--- src/components/teaser_volunteers.vue
+++ src/components/teaser_volunteers.vue
@@ -1,58 +1,60 @@
 <template>
-    <teaser_area :teaser="teaser"/>
+  <teaser_area :teaser="teaser" />
 </template>
 
 <script>
-    import Teaser_area from '@/components/teaser_area'
-    import ytIcon from '@/static/youtube-brands.svg'
+// eslint-disable-next-line camelcase
+import teaser_area from '@/components/teaser_area'
+import hundredIcon from '@/assets/icons/icon_100.svg?raw'
+import handsIcon from '@/assets/icons/icon_hands.svg?raw'
+import affeIcon from '../assets/icons/icon_affe.svg?raw'
 
-    export default {
-        name: 'teaser_volunteers',
-        components: { Teaser_area, ytIcon },
-        data() {
-            return {
-                teaser: {
-                    info: {
-                        text: 'DU KANNST HELFEN!'
-                    },
-                    header: {
-                        headline: 'Die Tafeln suchen...' //,
-                        // text: 'Du willst den Tafeln helfen, weißt aber nicht wo und wie?'
-                    },
-                    item: [
-                        {
-                            id: 0,
-                            icon: {
-                                url: '/youtube-brands.svg'
-                            },
-                            content: {
-                                headline: '… Deine Power',
-                                text: 'Die Lebensmittel für die Tafeln müssen natürlich erstmal abgeholt werden. Hier benötigst Du nicht nur Ausdauer und Kraft, sondern auch ein gutes Auge, um in dem Kühltransporter so viele Kisten wie möglich unterzubringen. Das perfekte Workout für Körper und Geist.'
-                            }
-                        },
-                        {
-                            id: 1,
-                            icon: {
-                                url: ''
-                            },
-                            content: {
-                                headline: '… Deine Organisation',
-                                text: 'Wenn die Lebensmittel bei der Tafel ankommen, gilt es gründlich zu sortieren. Verdorbene Lebensmittel müssen entsorgt werden. Genießbare Lebensmittel bekommen eine reinigende Dusche und werden für die Verteilung vorverpackt.'
-                            }
-                        },
-                        {
-                            id: 2,
-                            icon: {
-                                url: ''
-                            },
-                            content: {
-                                headline: '… Dein Lächeln',
-                                text: 'Aktuell sind die Tafeln darauf angewiesen, dass die Lebensmittel zu den Bedürftigen gebracht werden können. Und vor allem, wenn man nicht mit einem freundlichen Händedruck “Danke” sagen kann, ist ein Lächeln unbezahlbar.'
-                            }
-                        }
-                    ]
-                }
+export default {
+  name: 'TeaserVolunteers',
+  components: {
+    teaser_area
+  },
+  data() {
+    return {
+      teaser: {
+        info: {
+          text: 'DU KANNST HELFEN!'
+        },
+        header: {
+          headline: 'Die Tafeln suchen...' //,
+          // text: 'Du willst den Tafeln helfen, weißt aber nicht wo und wie?'
+        },
+        item: [
+          {
+            id: 0,
+            icon: hundredIcon,
+            content: {
+              headline: '… Deine Power',
+              text:
+                'Die Lebensmittel für die Tafeln müssen natürlich erstmal abgeholt werden. Hier benötigst Du nicht nur Ausdauer und Kraft, sondern auch ein gutes Auge, um in dem Kühltransporter so viele Kisten wie möglich unterzubringen. Das perfekte Workout für Körper und Geist.'
             }
-        }
+          },
+          {
+            id: 1,
+            icon: handsIcon,
+            content: {
+              headline: '… Deine Organisation',
+              text:
+                'Wenn die Lebensmittel bei der Tafel ankommen, gilt es gründlich zu sortieren. Verdorbene Lebensmittel müssen entsorgt werden. Genießbare Lebensmittel bekommen eine reinigende Dusche und werden für die Verteilung vorverpackt.'
+            }
+          },
+          {
+            id: 2,
+            icon: affeIcon,
+            content: {
+              headline: '… Dein Lächeln',
+              text:
+                'Aktuell sind die Tafeln darauf angewiesen, dass die Lebensmittel zu den Bedürftigen gebracht werden können. Und vor allem, wenn man nicht mit einem freundlichen Händedruck “Danke” sagen kann, ist ein Lächeln unbezahlbar.'
+            }
+          }
+        ]
+      }
     }
+  }
+}
 </script>
