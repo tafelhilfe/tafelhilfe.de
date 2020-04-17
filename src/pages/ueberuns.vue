98422596ec4cbccf64df5135b3dc6be351b231c1
Lennart Gastler <lennart.gastler@debab.de>
2020-04-18 00:05:09 +0200

    Added AboutUs and Influencer Pages

---
+++ src/pages/ueberuns.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="content container">
+    <div class="mx-auto py-6 mt-8">
+      <div class="px-4 py-4">
+        <h1>Wie ist <span class="text-secondary">#tafelhilfe</span> entstanden?</h1>
+        <h3>Das Team #tafelhilfe ist im Rahmen des #WirVsVirus-Hackathons im März 2020 entstanden.</h3>
+        <p class="paragraph">In Folge der COVID-19 Pandemie haben sich weltweit gesellschaftliche Probleme ergeben, für die es während des 48-Stunden-Hackathons galt, Lösungskonzepte zu finden.</p>
+        <p class="paragraph">
+          Von den Auswirkungen der Krise sind vor Allem die Menschen betroffen, die auch vorher auf die Hilfe anderer angewiesen waren.
+        </p>
+        <p class="paragraph">
+          In Deutschland sind es die lokalen Tafeln, die sicherstellen, dass auch Bedürftige regelmäßig mit Lebensmitteln versorgt werden. Sie leben von freiwilligen Helfer*innen, die die Lebensmittel abholen, sortieren und austeilen. Jedoch sind die meisten dieser Helfer*innen selbst ältere Menschen, die zur COVID-19 Risikogruppe gehören. Wenn diese Helfer*innen zu ihrer eigenen Sicherheit zu Hause bleiben, haben viele Tafeln nicht mehr genügend Unterstützung und müssen schließen.
+        </p>
+        <p class="paragraph">
+          Unser Ziel ist die Verbindung von motivierten, jungen Helfer*innen mit Tafeln in ihrer Umgebung, um krisenbedingte Schließungen zu verhindern oder rückgängig zu machen.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ueberuns",
+}
+</script>
+
+<style scoped>
+.paragraph {
+  font-size: 1.25rem !important;
+}
+</style>
